e3976f5992bac5a00af277bfdaf88b607194389f
Artin <122741147+ArtinMoghadasi@users.noreply.github.com>
2023-01-22 12:59:45 +0330

    Add files via upload

---
+++ HeartIcon.py
@@ -0,0 +1,16 @@
+from turtle import *
+import time
+color("red")
+bgcolor("purple")
+begin_fill()
+pensize(5)
+left(50)
+forward(133)
+circle(50,200)
+right(140)
+circle(50,200)
+forward(133)
+end_fill()
+time.sleep(5)
+print("The program is finished")
+#The program is finished
